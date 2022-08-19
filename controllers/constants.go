172a5c858846cde5517fafb84a4309079fe35372
Lukasz Zajaczkowski <zreigz@gmail.com>
2022-08-19 10:35:59 +0200

    add redeploy secret controller

--- controllers/constants.go
+++ controllers/constants.go
@@ -1,6 +1,8 @@
 package controllers
 
 const (
-	managedLabel     = "app.kubernetes.io/managed-by"
-	ignoreAnnotation = "platform.plural.sh/ignore"
+	managedLabel                = "app.kubernetes.io/managed-by"
+	ignoreAnnotation            = "platform.plural.sh/ignore"
+	shaAnnotation               = "platform.plural.sh/sha"
+	deploymentRestartAnnotation = "kubectl.kubernetes.io/restartedAt"
 )
