961d2fd2e67723926e5b6400d4a867743c18a6b4
Lukasz Zajaczkowski <zreigz@gmail.com>
2022-09-12 13:32:52 +0200

    add pod sweeper controller

--- main.go
+++ main.go
@@ -198,6 +198,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.PodSweeperReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+		Log:    ctrl.Log.WithName("controllers").WithName("PodSweeperController"),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "PodSweeperController")
+		os.Exit(1)
+	}
 	// //+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
